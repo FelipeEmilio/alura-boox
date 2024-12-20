9cf83b51838d1c7e303d272eb88de88cf380a002
Felipe Emílio <felipeemilio27@gmail.com>
2024-12-20 16:43:09 -0300

    onBlur e useState

--- src/Componentes/Pesquisa/Pesquisa.js
+++ src/Componentes/Pesquisa/Pesquisa.js
@@ -1,13 +1,19 @@
 import './Pesquisa.css'
 import Input from '../Input/index'
+import { useState } from 'react'
 
 export const Pesquisa = () => {
+
+    const [TextoDigitado, setTextoDigitado] = useState('')
 
     return(
         <section className='pesquisa'>
             <h2 className='titulo'>Já sabe por onde começar?</h2>
             <h3 className='subtitulo'>Encontre seu livro em nossa estante.</h3>
-            <Input placeholder='Escrava a sua próxima leitura...' />
+            <Input 
+                placeholder='Escrava a sua próxima leitura...' 
+                onBlur={evento => setTextoDigitado(evento.target.value)}
+            />
         </section>
     )
 
