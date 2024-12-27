3dee51892ca0c2b08030b472af681774fe86792f
Felipe Emílio <felipeemilio27@gmail.com>
2024-12-27 12:07:16 -0300

    ajustando o input

--- src/Componentes/Input/Input.js
+++ src/Componentes/Input/Input.js
@@ -7,10 +7,12 @@ export const Input = styled.input`
         border: 1px solid #FFF;
         padding: 20px 140px;
         border-radius: 50px;
-        width: 200px;
+        width: 222px;
+        text-align: center;
         color: #FFF;
         font-size: 16px;
         margin-bottom: 10px;
+        
 
         &::placeholder {
                 color: #FFF;
